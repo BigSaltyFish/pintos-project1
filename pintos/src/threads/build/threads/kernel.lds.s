a2ed9145d700c35f1883657e05e8d27acc8d11e7
wuping <2983483048@qq.com>
2018-12-31 12:21:12 +0800

    origin commit

---
+++ pintos/src/threads/build/threads/kernel.lds.s
@@ -0,0 +1,20 @@
+OUTPUT_FORMAT("elf32-i386")
+OUTPUT_ARCH("i386")
+ENTRY(start)
+SECTIONS
+{
+  . = 0xc0000000 + 0x100000;
+  _start = .;
+  .text : { *(.start) *(.text) } = 0x90
+  .rodata : { *(.rodata) *(.rodata.*)
+       . = ALIGN(0x1000);
+       _end_kernel_text = .; }
+ .eh_frame : { *(.eh_frame) }
+  .data : { *(.data)
+  _signature = .; LONG(0xaa55aa55)}
+ .plt : { *(.plt*) }
+  _start_bss = .;
+  .bss : { *(.bss) }
+  _end_bss = .;
+  _end = .;
+}
